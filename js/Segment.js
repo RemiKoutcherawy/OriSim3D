c1196d21ad3dfbe326bea4cf4e50e6251cf3ce99
remi <remi.koutcherawy@gmail.com>
2025-08-31 13:31:50 +0200

    Fix bisector2d

--- js/Segment.js
+++ js/Segment.js
@@ -125,10 +125,10 @@ export class Segment {
         const v2_x = d.xf - c.xf;
         const v2_y = d.yf - c.yf;
         const determinant = -v2_x * v1_y + v1_x * v2_y;
-        if (Math.round(determinant) < 1e-3) {
+        if (Math.abs(determinant) < 1e-10) {
             return undefined;
         }
-        // let s = (-v1_y * (a.xf - c.xf) + v1_x * (a.yf - c.yf)) / determinant;
+        // const s = (-v1_y * (a.xf - c.xf) + v1_x * (a.yf - c.yf)) / determinant;
         const t = (v2_x * (a.yf - c.yf) - v2_y * (a.xf - c.xf)) / determinant;
         const xf = a.xf + (t * v1_x);
         const yf = a.yf + (t * v1_y);
